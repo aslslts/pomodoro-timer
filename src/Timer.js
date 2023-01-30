66ed4da5c4d1ec06a72f2db1aac5dc9246808dc5
Aslı Sulutaş <cemree.1402@gmail.com>
2023-01-30 19:52:03 +0300

    fix

--- src/Timer.js
+++ src/Timer.js
@@ -2,10 +2,21 @@ import React from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
+const red = "#f54e4e";
+const green = "#4aec8c";
+
 function Timer() {
   return (
     <div>
-      <CircularProgressbar value={60} text={`60%`} />
+      <CircularProgressbar
+        value={60}
+        text={`60%`}
+        styles={buildStyles({
+          textColor: "#fff",
+
+          tailColor: "rgba(255,255,255,.2)",
+        })}
+      />
     </div>
   );
 }
