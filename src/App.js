2815ab3ab80c6fd88348aa396bd28e76d92c9abc
Aslı Sulutaş <cemree.1402@gmail.com>
2023-02-02 14:12:06 +0300

    add Setting js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Settings from "./Settings";
 import Timer from "./Timer";
 
+import { useState } from "react";
+
 function App() {
+
+const  [showSettings,setShowSettings] =useState(initialState: true);
+
   return (
     <main>
-      <Timer />
+      {showSettings ? <Settings /> : <Timer />}
     </main>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -3,6 +3,7 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import PlayButton from "./PlayButton";
 import PauseButton from "./PauseButton";
+import SettingsButton from "./SettingsButton";
 
 const red = "#f54e4e";
 const green = "#4aec8c";
@@ -24,7 +25,9 @@ function Timer() {
         <PlayButton />
         <PauseButton />
       </div>
-      <div style={{ marginTop: "20px" }}></div>
+      <div style={{ marginTop: "20px" }}>
+        <SettingsButton />
+      </div>
     </div>
   );
 }
