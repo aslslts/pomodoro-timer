126eb1957fcf69c3919e0b7f3c73b5432affe1c8
Aslı Sulutaş <cemree.1402@gmail.com>
2023-02-21 22:08:56 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import Settings from "./Settings";
 import Timer from "./Timer";
-
+import Settings from "./Settings";
 import { useState } from "react";
+import SettingsContext from "./SettingsContext";
 
 function App() {
-
-const  [showSettings,setShowSettings] =useState(initialState: true);
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
     <main>
-      {showSettings ? <Settings /> : <Timer />}
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
     </main>
   );
 }
